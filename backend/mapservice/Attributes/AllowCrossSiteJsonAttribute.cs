caa66b5d509466392fbf57dcbc7f8af4ffe18e37
Mattias Andrén <mattias.andren@zinet.se>
2021-12-20 15:03:00 +0100

    Added possibility to turn off CORS Headers using a C# compilation symbol.

--- backend/mapservice/Attributes/AllowCrossSiteJsonAttribute.cs
+++ backend/mapservice/Attributes/AllowCrossSiteJsonAttribute.cs
@@ -8,6 +8,7 @@ namespace MapService.Attributes
 {
 	public class CORSActionFilter : ActionFilterAttribute
 	{
+#if !DISABLE_CORS_IN_CODE // To disable CORS Headers, add DISABLE_CORS_IN_CODE at Project mapservice->Properties->Build->Conditional compilation symbols
 		public override void OnActionExecuting(ActionExecutingContext filterContext)
 		{
             filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*");
@@ -23,5 +24,6 @@ namespace MapService.Attributes
 				base.OnActionExecuting(filterContext);
 			}
 		}
+#endif
 	}
 }
